052149a0fa4e63106f2495669818a81856998e8c
shohdi <shohdi@gmail.com>
2019-03-29 16:23:06 +0300

    add all files , need to moved to folder

---
+++ frmSearch.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SearchInFiles
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.chkEnableDate = new System.Windows.Forms.CheckBox();
+            this.lstSearchResult = new System.Windows.Forms.ListBox();
+            this.tmrUpdateList = new System.Windows.Forms.Timer(this.components);
+            this.lblFile = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkKeepPath = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(494, 295);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dtFrom.Enabled = false;
+            this.dtFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtFrom.Location = new System.Drawing.Point(120, 80);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(187, 20);
+            this.dtFrom.TabIndex = 1;
+            // 
+            // dtTo
+            // 
+            this.dtTo.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dtTo.Enabled = false;
+            this.dtTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtTo.Location = new System.Drawing.Point(341, 80);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(200, 20);
+            this.dtTo.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(414, 21);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(45, 24);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(345, 20);
+            this.txtDirectory.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(45, 142);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(444, 106);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // chkEnableDate
+            // 
+            this.chkEnableDate.AutoSize = true;
+            this.chkEnableDate.Location = new System.Drawing.Point(49, 80);
+            this.chkEnableDate.Name = "chkEnableDate";
+            this.chkEnableDate.Size = new System.Drawing.Size(59, 17);
+            this.chkEnableDate.TabIndex = 6;
+            this.chkEnableDate.Text = "Enable";
+            this.chkEnableDate.UseVisualStyleBackColor = true;
+            this.chkEnableDate.CheckedChanged += new System.EventHandler(this.chkEnableDate_CheckedChanged);
+            // 
+            // lstSearchResult
+            // 
+            this.lstSearchResult.FormattingEnabled = true;
+            this.lstSearchResult.Location = new System.Drawing.Point(45, 350);
+            this.lstSearchResult.Name = "lstSearchResult";
+            this.lstSearchResult.ScrollAlwaysVisible = true;
+            this.lstSearchResult.Size = new System.Drawing.Size(507, 212);
+            this.lstSearchResult.TabIndex = 7;
+            this.lstSearchResult.SelectedIndexChanged += new System.EventHandler(this.lstSearchResult_SelectedIndexChanged);
+            this.lstSearchResult.DoubleClick += new System.EventHandler(this.lstSearchResult_DoubleClick);
+            // 
+            // tmrUpdateList
+            // 
+            this.tmrUpdateList.Enabled = true;
+            this.tmrUpdateList.Interval = 1000;
+            this.tmrUpdateList.Tick += new System.EventHandler(this.tmrUpdateList_Tick);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(46, 260);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(35, 13);
+            this.lblFile.TabIndex = 8;
+            this.lblFile.Text = "label1";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(391, 295);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // chkKeepPath
+            // 
+            this.chkKeepPath.AutoSize = true;
+            this.chkKeepPath.Checked = true;
+            this.chkKeepPath.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkKeepPath.Location = new System.Drawing.Point(45, 119);
+            this.chkKeepPath.Name = "chkKeepPath";
+            this.chkKeepPath.Size = new System.Drawing.Size(76, 17);
+            this.chkKeepPath.TabIndex = 10;
+            this.chkKeepPath.Text = "Keep Path";
+            this.chkKeepPath.UseVisualStyleBackColor = true;
+            // 
+            // frmSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 590);
+            this.Controls.Add(this.chkKeepPath);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lstSearchResult);
+            this.Controls.Add(this.chkEnableDate);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.dtTo);
+            this.Controls.Add(this.dtFrom);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmSearch";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.frmSearch_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.CheckBox chkEnableDate;
+        private System.Windows.Forms.ListBox lstSearchResult;
+        private System.Windows.Forms.Timer tmrUpdateList;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkKeepPath;
+    }
+}
+
---
+++ frmSearch.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchInFiles
+{
+    public partial class frmSearch : Form
+    {
+        private string lastFile = "";
+        private int countFiles = 0;
+        List<FileInfo> lstFiles = new List<FileInfo>();
+        private bool started = false;
+        public frmSearch()
+        {
+            InitializeComponent();
+        }
+
+        private string folder = "";
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog ();
+            var dialogResult = dialog.ShowDialog();
+            if(dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                this.folder = new FileInfo( dialog.FileName).Directory.FullName;
+            }
+
+
+            txtDirectory.Text = this.folder;
+        }
+
+        private void chkEnableDate_CheckedChanged(object sender, EventArgs e)
+        {
+            
+                dtFrom.Enabled = chkEnableDate.Checked;
+                dtTo.Enabled = chkEnableDate.Checked;
+            
+        }
+
+        private void frmSearch_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Thread thThread = null;
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace( txtSearch.Text) && !string.IsNullOrWhiteSpace(txtDirectory.Text))
+            {
+                started = true;
+                btnSearch.Enabled = false;
+
+                lstFiles = new List<FileInfo>();
+                this.folder = txtDirectory.Text;
+                DateTime? fromDate = null;
+                DateTime? toDate = null;
+                if(chkEnableDate.Checked)
+                {
+                    fromDate = dtFrom.Value;
+                    toDate = dtTo.Value;
+                }
+                Regex regSearch = new Regex("");
+                try
+                {
+                    regSearch = new Regex(txtSearch.Text, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                }
+                catch
+                {
+                    started = false;
+                    return;
+                }
+                
+                thThread = new Thread(() => {
+                    lstFiles = searchDir(this.folder, fromDate, toDate, regSearch,chkKeepPath.Checked, lstFiles);
+                    started = false;
+                    var dirName = DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss");
+                    Directory.CreateDirectory(".\\" + dirName);
+                    foreach (var file in lstFiles)
+                    {
+                        try
+                        {
+                            Regex nameTester = new Regex("[\\\\\\/\\:\\*\\?\\\"\\<\\>\\|]+");
+                            string fileName = "";
+                            if (chkKeepPath.Checked)
+                                fileName = nameTester.Replace(file.FullName, "_");
+                            else
+                                fileName = file.Name;
+                            fileName = Path.Combine(".\\" + dirName, fileName);
+                            File.Copy(file.FullName, fileName);
+
+                        }
+                        catch(Exception ex)
+                        {
+
+                        }
+                    }
+
+                });
+
+                thThread.Start();
+                
+            }
+
+        }
+
+        private List<FileInfo> searchDir(string dir, DateTime? fromDate, DateTime? toDate, Regex regSearch,bool keepPath, List<FileInfo> outFiles = null)
+        {
+            List<FileInfo> ret = new List<FileInfo>();
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+            DirectoryInfo[] dirs = dirInfo.GetDirectories();
+            foreach(var inDir in dirs)
+            {
+                var lstFoundFiles = searchDir(inDir.FullName, fromDate, toDate, regSearch,keepPath,outFiles);
+                ret.AddRange(lstFoundFiles);
+            }
+            FileInfo[] files = dirInfo.GetFiles();
+            foreach(var f in files)
+            {
+                lastFile = f.FullName;
+                if (fromDate != null && toDate != null && (f.LastWriteTime < fromDate.Value || f.LastWriteTime > toDate.Value))
+                {
+                    
+                }
+                else
+                {
+                    string fileData = File.ReadAllText(f.FullName);
+                    if(regSearch.IsMatch(fileData))
+                    {
+                        ret.Add(f);
+                        if(outFiles != null)
+                        {
+                            outFiles.Add(f);
+                        }
+                    }
+                }
+            }
+
+            return ret;
+
+        }
+
+        private void tmrUpdateList_Tick(object sender, EventArgs e)
+        {
+            lblFile.Text = lastFile;
+            if(lstFiles.Count != countFiles)
+            {
+                countFiles = lstFiles.Count;
+                List<string> lstOutput = lstFiles.Select(l => l.FullName).ToList();
+                lstSearchResult.DataSource = null;
+                lstSearchResult.Refresh();
+                lstSearchResult.DataSource = lstOutput;
+                lstSearchResult.Refresh();
+
+            }
+
+            if(!started)
+            {
+                btnSearch.Enabled = !started;
+            }
+        }
+
+        private void lstSearchResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstSearchResult_DoubleClick(object sender, EventArgs e)
+        {
+            if(lstSearchResult.SelectedValue != null)
+            {
+                string val = lstSearchResult.SelectedValue.ToString();
+                if(!string.IsNullOrWhiteSpace(val))
+                {
+                    Process.Start(val);
+                    
+                }
+            }
+
+           
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thThread.Abort();
+
+            }
+            catch
+            {
+
+            }
+
+            started = false;
+        }
+    }
+}
